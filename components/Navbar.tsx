cf945d8c54f22876b22a1e19e8b1e0bcd9d2c75d
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-03-30 20:59:27 +0300

    fix: position & border

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar: FC = () => {
     }
 
     return (
-        <nav className="bg-white border-b border-gray-200 dark:bg-gray-900">
+        <nav className="fixed top-0 left-0 w-full h-fit z-50 bg-white border-b border-gray-200 dark:border-gray-600 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a
                     href="#"
