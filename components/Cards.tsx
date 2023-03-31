f30f3574cac8eb5e3e3550e4cc639820f6016da7
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-03-31 19:31:30 +0300

    feat: cards

---
+++ components/Cards.tsx
@@ -0,0 +1,98 @@
+import { type FC } from 'react';
+import Image from 'next/image';
+
+const Cards: FC = () => {
+
+    return (
+        <article className='relative mt-40 mb-20 mx-5 flex flex-nowrap gap-10 overflow-x-auto justify-start lg:justify-center items-center scrollbar-hide'>
+            <div
+                className="block min-w-[15rem] w-[30rem] p-6 bg-white border border-gray-200 rounded-2xl shadow
+                        dark:bg-gray-800 dark:border-gray-700"
+            >
+                <a
+                    href='#'
+                    className="mb-2 text-2xl tracking-tight text-gray-900 dark:text-white flex gap-3 items-center 
+                                justify-center hover:gap-5 transition-all duration-200 rounded-lg
+                                focus:outline-none focus:border-0 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-600"
+                >
+                    <b>Business Rentals</b>
+                    <span
+                        aria-hidden
+                        className='w-3 h-2.5 bg-no-repeat bg-[url(/icons/chev.svg)] dark:bg-[url(/icons/chev-dark.svg)]'
+                    ></span>
+                </a>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Our customized rental programs are designed specifically to meet your company’s needs, saving you time and money.
+                </p>
+                <div className='relative block h-[10rem] mt-5' aria-label='Image container'>
+                    <Image
+                        src='/images/cars.jpg'
+                        alt='Industrial parking lot filled with cars'
+                        fill
+                        className='object-cover'
+                    />
+                </div>
+            </div>
+            <div
+                className="block min-w-[15rem] w-[30rem] p-6 bg-white border border-gray-200 rounded-2xl shadow
+                        dark:bg-gray-800 dark:border-gray-700"
+            >
+                <a
+                    href='#'
+                    className="mb-2 text-2xl tracking-tight text-gray-900 dark:text-white flex gap-3 items-center 
+                                justify-center hover:gap-5 transition-all duration-200 rounded-lg
+                                focus:outline-none focus:border-0 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-600"
+                >
+                    <b>Replacement Rentals</b>
+                    <span
+                        aria-hidden
+                        className='w-3 h-2.5 bg-no-repeat bg-[url(/icons/chev.svg)] dark:bg-[url(/icons/chev-dark.svg)]'
+                    ></span>
+                </a>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Is your vehicle in the shop? After an accident the average time for repair is two weeks. We have the vehicle you need to keep your life moving.
+                </p>
+                <div className='relative block h-[10rem] mt-5' aria-label='Image container'>
+                    <Image
+                        src='/images/mechanic.jpg'
+                        alt='Car mechanic working under a car'
+                        fill
+                        className='object-cover'
+                    />
+                </div>
+            </div>
+            <div
+                className="block min-w-[15rem] w-[30rem] p-6 bg-white border border-gray-200 rounded-2xl shadow
+                        dark:bg-gray-800 dark:border-gray-700"
+            >
+                <a
+                    href='#'
+                    className="mb-2 text-2xl tracking-tight text-gray-900 dark:text-white flex gap-3 items-center 
+                                justify-center hover:gap-5 transition-all duration-200 rounded-lg
+                                focus:outline-none focus:border-0 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-600"
+                >
+                    <b>Current Deals</b>
+                    <span
+                        aria-hidden
+                        className='w-3 h-2.5 bg-no-repeat bg-[url(/icons/chev.svg)] dark:bg-[url(/icons/chev-dark.svg)]'
+                    ></span>
+                </a>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Explore our current deals and promotions or start a reservation to find the right vehicle at everyday low rates.
+                </p>
+                <div className='relative block h-[10rem] mt-5' aria-label='Image container'>
+                    <Image
+                        src='/images/car-keys.jpg'
+                        alt='Car keys'
+                        fill
+                        className='object-cover'
+                    />
+                </div>
+            </div>
+
+        </article>
+    );
+
+}
+
+export default Cards;
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,12 +6,12 @@ import MainForm from './MainForm';
 const Header: FC = () => {
 
     return (
-        <header className='relative top-[4.5rem]'>
+        <header className='block relative top-[4.5rem] mb-20'>
             <div className='relative w-full h-[20rem] lg:h-[25rem] overflow-hidden'>
                 <Image src={Background} alt='Picture of a curvy road from above' className='absolute top-0 left-0 h-full object-cover' />
             </div>
             <MainForm />
-            <ul className='mb-20 mx-5 flex flex-col md:flex-row justify-center items-center gap-20 text-black font-medium dark:text-white'>
+            <ul className='mx-5 flex flex-col md:flex-row justify-center items-center gap-20 text-black font-medium dark:text-white'>
                 <li className='flex items-center gap-5'>
                     <Image src='/icons/best-price.svg' alt='Price tag icon' width={70} height={40} />
                     <p>
@@ -25,7 +25,7 @@ const Header: FC = () => {
                     </p>
                 </li>
                 <li className='flex items-center gap-5'>
-                    <Image src='/icons/steps.svg' alt='Price tag icon' width={70} height={40} />
+                    <Image src='/icons/steps.svg' alt='Price tag icon' width={40} height={40} />
                     <p>
                         Book in 3 easy steps
                     </p>
